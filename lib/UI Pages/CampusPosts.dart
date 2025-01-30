7ff8b50adda3a41b0e6d66509b7db7872fa80c36
Rayyan-6 <muhd.rayyan.mail@gmail.com>
2025-01-30 16:55:23 +0500

    Posts UI updated

--- lib/UI Pages/CampusPosts.dart
+++ lib/UI Pages/CampusPosts.dart
@@ -5,7 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'MyDrawer.dart';
 import 'package:beyondtheclass/features/auth/presentation/auth_screen.dart';
 import 'package:beyondtheclass/features/auth/providers/auth_provider.dart';
-
+import 'package:intl/intl.dart'; // For date formatting
 
 class CampusPosts extends ConsumerWidget {
   const CampusPosts({super.key});
@@ -131,134 +131,160 @@ class CampusPosts extends ConsumerWidget {
                   delegate: SliverChildBuilderDelegate(
                         (context, index) {
                       final post = postList[index];
-                      return Card(
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        margin: const EdgeInsets.symmetric(vertical: 12),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                Colors.teal.shade900,
-                                Colors.tealAccent.shade400,
-                              ],
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                            ),
+                      final createdAt = DateTime.parse(post['createdAt']);
+                      final formattedDate = DateFormat('MMM d, y').format(createdAt); // Format the date
+                      final upVotesCount = post['voteId']?['upVotesCount'] ?? 0;
+                      final downVotesCount = post['voteId']?['downVotesCount'] ?? 0;
+                      final commentsCount = post['commentsCount'] ?? 0;
+
+                      return Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: Card(
+                          elevation: 5,
+                          shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(16),
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(12),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  children: [
-                                    CircleAvatar(
-                                      backgroundColor: Colors.white,
-                                      backgroundImage: post['author'] != null && post['author']['profile'] != null
-                                          ? NetworkImage(post['author']['profile']['picture'] ?? '')
-                                          : AssetImage('assets/default_profile_picture.png'), // Default image if null
-                                    ),
-                                    const SizedBox(width: 10),
-                                    Expanded(
-                                      child: Row(
+                          margin: const EdgeInsets.symmetric(vertical: 12),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Colors.teal.shade900,
+                                  Colors.tealAccent.shade400,
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(12),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      CircleAvatar(
+                                        backgroundColor: Colors.white,
+                                        backgroundImage: post['author'] != null && post['author']['profile'] != null
+                                            ? NetworkImage(post['author']['profile']['picture'] ?? '')
+                                            : AssetImage('assets/default_profile_picture.png'), // Default image if null
+                                      ),
+                                      const SizedBox(width: 10),
+                                      Expanded(
+                                        child: Row(
+                                          children: [
+                                            Column(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  post['author']?['name'] ?? '{Deleted}', // Null-aware operator used here
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 15,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  '@${post['author']?['username'] ?? ''}', // Null-aware operator and null-coalescing
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 12,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                        
+                                          ],
+                                        ),
+                                      ),
+                                      const Icon(
+                                        Icons.more_vert,
+                                        color: Colors.white,
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    post['title'] ?? '',
+                                    style: TextStyle(fontSize: 14,color: Colors.white,fontWeight: FontWeight.bold),
+                                    maxLines: 1,
+                        
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(
+                                    post['body'] ?? '',
+                                    style: TextStyle(fontSize: 13,color: Colors.white),
+                                    maxLines: 4,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
                                         children: [
-                                          Column(
-                                            mainAxisAlignment: MainAxisAlignment.start,
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                post['author']?['name'] ?? '{Deleted}', // Null-aware operator used here
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 18,
-                                                ),
-                                              ),
-                                              Text(
-                                                '@${post['author']?['username'] ?? ''}', // Null-aware operator and null-coalescing
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 12,
-                                                ),
-                                              ),
-                                            ],
+                                          Icon(
+                                            Icons.favorite_border,
+                                            color: Colors.white,
+                                            size: 20,
                                           ),
-
+                                          const SizedBox(width: 6),
+                                          Text(
+                                            '$upVotesCount', // Display upvotes count
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall
+                                                ?.copyWith(color: Colors.white),
+                                          ),
                                         ],
                                       ),
-                                    ),
-                                    const Icon(
-                                      Icons.more_vert,
-                                      color: Colors.white,
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 8),
-                                Text(
-                                  post['title'] ?? '',
-                                  style: TextStyle(fontSize: 18,color: Colors.white,fontWeight: FontWeight.bold),
-                                  maxLines: 1,
-
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                Text(
-                                  post['body'] ?? '',
-                                  style: TextStyle(fontSize: 14,color: Colors.white),
-                                  maxLines: 4,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                const SizedBox(height: 10),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Icon(
-                                          Icons.favorite_border,
-                                          color: Colors.white,
-                                          size: 20,
-                                        ),
-                                        const SizedBox(width: 6),
-                                        Text(
-                                          'Like',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .bodySmall
-                                              ?.copyWith(color: Colors.white),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: [
-                                        Icon(
-                                          Icons.comment,
-                                          color: Colors.white,
-                                          size: 20,
-                                        ),
-                                        const SizedBox(width: 6),
-                                        Text(
-                                          'Comment',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .bodySmall
-                                              ?.copyWith(color: Colors.white),
-                                        ),
-                                      ],
-                                    ),
-                                    Text(
-                                      '2 hours ago',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodySmall
-                                          ?.copyWith(color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.thumb_down_alt_outlined,
+                                            color: Colors.white,
+                                            size: 20,
+                                          ),
+                                          const SizedBox(width: 6),
+                                          Text(
+                                            '$downVotesCount', // Display downvotes count
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall
+                                                ?.copyWith(color: Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.comment,
+                                            color: Colors.white,
+                                            size: 20,
+                                          ),
+                                          const SizedBox(width: 6),
+                                          Text(
+                                            '$commentsCount', // Display comments count
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodySmall
+                                                ?.copyWith(color: Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                      Text(
+                                        formattedDate, // Display formatted date
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodySmall
+                                            ?.copyWith(color: Colors.white),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
@@ -275,4 +301,5 @@ class CampusPosts extends ConsumerWidget {
     );
   }
 }
+
 
