97dec1d0735ad6e35bff6636224282873437ef53
Rayyan-6 <muhd.rayyan.mail@gmail.com>
2025-07-12 05:14:51 +0500

    post issues. gps done

--- lib/shared/utils/constants.dart
+++ lib/shared/utils/constants.dart
@@ -35,7 +35,7 @@ class ApiConstants {
     111: "http://10.135.54.210:8080"
   };
 
-  static int _currentUrlIndex = 6;
+  static int _currentUrlIndex = -2;
   static const String _urlIndexCacheKey = 'current_url_index';
 
   static Future<void> initializeBaseUrl() async {
