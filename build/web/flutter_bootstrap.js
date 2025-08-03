1249b75188ee455969eab93787d695218f48bd4e
Muhammad Bilal Ellahi <bilalillahi25@gmail.com>
2025-08-03 07:49:37 +0500

    stuff

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3859291449"
+    serviceWorkerVersion: "3736861574"
   }
 });
